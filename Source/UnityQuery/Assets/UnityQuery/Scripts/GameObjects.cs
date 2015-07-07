fe08c3037fa3367494e91f1fd66b3f7520bbca7d
Nick Prühs <dev@npruehs.de>
2015-07-07 21:02:58 +0200

    ADDED: Game Objects - Add child.

---
+++ Source/UnityQuery/Assets/UnityQuery/Scripts/GameObjects.cs
@@ -0,0 +1,66 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GameObjects.cs" company="Nick Prühs">
+//   Copyright (c) Nick Prühs. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace UnityQuery
+{
+    using UnityEngine;
+
+    public static class GameObjects
+    {
+        #region Public Methods and Operators
+
+        /// <summary>
+        ///   Instantiates a new game object and parents it to the specified one.
+        ///   Resets position, rotation and scale and inherits the layer.
+        /// </summary>
+        /// <param name="parent">Game object to add the child to.</param>
+        /// <returns>New child.</returns>
+        public static GameObject AddChild(this GameObject parent)
+        {
+            return parent.AddChild("GameObject");
+        }
+
+        /// <summary>
+        ///   Instantiates a new game object and parents it to the specified one.
+        ///   Resets position, rotation and scale and inherits the layer.
+        /// </summary>
+        /// <param name="parent">Game object to add the child to.</param>
+        /// <param name="name">Name of the child to add.</param>
+        /// <returns>New child.</returns>
+        public static GameObject AddChild(this GameObject parent, string name)
+        {
+            var go = AddChild(parent, (GameObject)null);
+            go.name = name;
+            return go;
+        }
+
+        /// <summary>
+        ///   Instantiates a prefab and parents it to the specified one.
+        ///   Resets position, rotation and scale and inherits the layer.
+        /// </summary>
+        /// <param name="parent">Game object to add the child to.</param>
+        /// <param name="prefab">Prefab to instantiate.</param>
+        /// <returns>New prefab instance.</returns>
+        public static GameObject AddChild(this GameObject parent, GameObject prefab)
+        {
+            var go = prefab != null ? Object.Instantiate(prefab) : new GameObject();
+            if (go == null || parent == null)
+            {
+                return go;
+            }
+
+            var transform = go.transform;
+            transform.parent = parent.transform;
+            transform.localPosition = Vector3.zero;
+            transform.localRotation = Quaternion.identity;
+            transform.localScale = Vector3.one;
+            go.layer = parent.layer;
+            return go;
+        }
+
+        #endregion
+    }
+}
