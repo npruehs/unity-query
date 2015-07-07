c28061a6db49f44bed60d37399bc6d6455569a9d
Nick Prühs <dev@npruehs.de>
2015-07-07 20:25:48 +0200

    ADDED: Vectors - Component-wise swizzling.

---
+++ Source/UnityQuery/Assets/UnityQuery.Tests/Editor/SwizzlingTests.cs
@@ -0,0 +1,71 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SwizzlingTests.cs" company="Nick Prühs">
+//   Copyright (c) Nick Prühs. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace UnityQuery.Tests
+{
+    using NUnit.Framework;
+
+    using UnityEngine;
+
+    public class SwizzlingTests
+    {
+        #region Constants
+
+        private const float X = 1;
+
+        private const float Y = 2;
+
+        private const float Z = 3;
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        [Test]
+        public void TestVectorXY()
+        {
+            // Arrange.
+            var u = new Vector3(X, Y, Z);
+
+            // Act.
+            var v = u.XY();
+
+            // Assert.
+            Assert.AreEqual(X, v.x);
+            Assert.AreEqual(Y, v.y);
+        }
+
+        [Test]
+        public void TestVectorXZ()
+        {
+            // Arrange.
+            var u = new Vector3(X, Y, Z);
+
+            // Act.
+            var v = u.XZ();
+
+            // Assert.
+            Assert.AreEqual(X, v.x);
+            Assert.AreEqual(Z, v.y);
+        }
+
+        [Test]
+        public void TestVectorYZ()
+        {
+            // Arrange.
+            var u = new Vector3(X, Y, Z);
+
+            // Act.
+            var v = u.YZ();
+
+            // Assert.
+            Assert.AreEqual(Y, v.x);
+            Assert.AreEqual(Z, v.y);
+        }
+
+        #endregion
+    }
+}
---
+++ Source/UnityQuery/Assets/UnityQuery/Scripts/Swizzling.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Swizzling.cs" company="Nick Prühs">
+//   Copyright (c) Nick Prühs. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace UnityQuery
+{
+    using UnityEngine;
+
+    public static class Swizzling
+    {
+        #region Public Methods and Operators
+
+        public static Vector2 XY(this Vector3 v)
+        {
+            return new Vector2(v.x, v.y);
+        }
+
+        public static Vector2 XZ(this Vector3 v)
+        {
+            return new Vector2(v.x, v.z);
+        }
+
+        public static Vector2 YZ(this Vector3 v)
+        {
+            return new Vector2(v.y, v.z);
+        }
+
+        #endregion
+    }
+}
