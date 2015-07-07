a46b0755c7f1cb76eb38e0dccfcbaa5b425dc9fe
Nick Prühs <dev@npruehs.de>
2015-07-07 20:39:50 +0200

    ADDED: Collections - ToString method.

---
+++ Source/UnityQuery/Assets/UnityQuery.Tests/Editor/CollectionsTests.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CollectionTests.cs" company="Nick Prühs">
+//   Copyright (c) Nick Prühs. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace UnityQuery.Tests
+{
+    using System.Collections.Generic;
+
+    using NUnit.Framework;
+
+    public class CollectionsTests
+    {
+        #region Public Methods and Operators
+
+        [Test]
+        public void TestSequenceToString()
+        {
+            // Arrange.
+            var list = new List<int> { 1, 2, 3 };
+
+            // Act.
+            var s = list.SequenceToString();
+
+            // Assert.
+            Assert.AreEqual("[1,2,3]", s);
+        }
+
+        #endregion
+    }
+}
---
+++ Source/UnityQuery/Assets/UnityQuery/Scripts/Collections.cs
@@ -0,0 +1,58 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Collections.cs" company="Nick Prühs">
+//   Copyright (c) Nick Prühs. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace UnityQuery
+{
+    using System.Collections;
+    using System.Text;
+
+    public static class Collections
+    {
+        #region Public Methods and Operators
+
+        public static string SequenceToString(this IEnumerable sequence)
+        {
+            // Check empty sequence.
+            if (sequence == null)
+            {
+                return "null";
+            }
+
+            var stringBuilder = new StringBuilder();
+
+            // Add opening bracket.
+            stringBuilder.Append("[");
+
+            foreach (var element in sequence)
+            {
+                var elementString = element as string;
+                if (elementString == null)
+                {
+                    // Handle nested enumerables.
+                    var elementEnumerable = element as IEnumerable;
+                    elementString = elementEnumerable != null
+                        ? elementEnumerable.SequenceToString()
+                        : element.ToString();
+                }
+
+                // Add comma.
+                stringBuilder.AppendFormat("{0},", elementString);
+            }
+
+            // Empty sequence.
+            if (stringBuilder.Length <= 1)
+            {
+                return "[]";
+            }
+
+            // Add closing bracket.
+            stringBuilder[stringBuilder.Length - 1] = ']';
+            return stringBuilder.ToString();
+        }
+
+        #endregion
+    }
+}
