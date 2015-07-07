6f7a5279c55384c75c9c9e68e45d080f91946b01
Nick Prühs <dev@npruehs.de>
2015-07-07 20:52:35 +0200

    ADDED: Collections - ContainsAll, GetValueOrDefault, IsNullOrEmpty.

--- Source/UnityQuery/Assets/UnityQuery.Tests/Editor/CollectionsTests.cs
+++ Source/UnityQuery/Assets/UnityQuery.Tests/Editor/CollectionsTests.cs
@@ -1,5 +1,5 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="CollectionTests.cs" company="Nick Prühs">
+// <copyright file="CollectionsTests.cs" company="Nick Prühs">
 //   Copyright (c) Nick Prühs. All rights reserved.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
@@ -15,7 +15,49 @@ namespace UnityQuery.Tests
         #region Public Methods and Operators
 
         [Test]
-        public void TestSequenceToString()
+        public void TestContainsAll()
+        {
+            // Arrange.
+            var first = new List<int> { 1, 2, 3 };
+            var second = new List<int> { 1, 3 };
+            var third = new List<int> { 1, 2, 4 };
+
+            // Assert.
+            Assert.IsTrue(first.ContainsAll(second));
+            Assert.IsFalse(first.ContainsAll(third));
+        }
+
+        [Test]
+        public void TestGetValueOrDefault()
+        {
+            // Arrange.
+            var dictionary = new Dictionary<string, int>();
+            dictionary.Add("one", 1);
+
+            // Assert.
+            Assert.AreEqual(1, dictionary.GetValueOrDefault("one", 0));
+
+            Assert.AreEqual(0, dictionary.GetValueOrDefault("two"));
+            Assert.AreEqual(0, dictionary.GetValueOrDefault("two", 0));
+        }
+
+        [Test]
+        public void TestNullOrEmpty()
+        {
+            // Arrange.
+            var sequence = new List<int> { 1, 2, 3 };
+            var emptySequence = new List<int>();
+            List<int> nullSequence = null;
+
+            // Assert.
+            Assert.IsFalse(sequence.IsNullOrEmpty());
+
+            Assert.IsTrue(emptySequence.IsNullOrEmpty());
+            Assert.IsTrue(nullSequence.IsNullOrEmpty());
+        }
+
+        [Test]
+        public void TestToString()
         {
             // Arrange.
             var list = new List<int> { 1, 2, 3 };
