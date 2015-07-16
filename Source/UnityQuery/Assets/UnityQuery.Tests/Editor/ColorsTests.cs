3f9880979472bfede1f68a24a65f83f9166c8e22
Nick Prühs <dev@npruehs.de>
2015-07-16 23:16:57 +0200

    ADDED: Colors - Methods for changing color values while preserving immutability.

---
+++ Source/UnityQuery/Assets/UnityQuery.Tests/Editor/ColorsTests.cs
@@ -0,0 +1,99 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ColorsTests.cs" company="Nick Prühs">
+//   Copyright (c) Nick Prühs. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace UnityQuery.Tests
+{
+    using NUnit.Framework;
+
+    using UnityEngine;
+
+    public class ColorsTests
+    {
+        #region Constants
+
+        private const float A = 0.4f;
+
+        private const float B = 0.3f;
+
+        private const float G = 0.2f;
+
+        private const float R = 0.1f;
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        [Test]
+        public void TestColorWithA()
+        {
+            // Arrange.
+            var before = new Color(R, G, B, A);
+            const float NewAlpha = 0.5f;
+
+            // Act.
+            var after = before.WithAlpha(NewAlpha);
+
+            // Assert.
+            Assert.AreEqual(before.r, after.r);
+            Assert.AreEqual(before.g, after.g);
+            Assert.AreEqual(before.b, after.b);
+            Assert.AreEqual(NewAlpha, after.a);
+        }
+
+        [Test]
+        public void TestColorWithB()
+        {
+            // Arrange.
+            var before = new Color(R, G, B, A);
+            const float NewBlue = 0.5f;
+
+            // Act.
+            var after = before.WithBlue(NewBlue);
+
+            // Assert.
+            Assert.AreEqual(before.r, after.r);
+            Assert.AreEqual(before.g, after.g);
+            Assert.AreEqual(NewBlue, after.b);
+            Assert.AreEqual(before.a, after.a);
+        }
+
+        [Test]
+        public void TestColorWithG()
+        {
+            // Arrange.
+            var before = new Color(R, G, B, A);
+            const float NewGreen = 0.5f;
+
+            // Act.
+            var after = before.WithGreen(NewGreen);
+
+            // Assert.
+            Assert.AreEqual(before.r, after.r);
+            Assert.AreEqual(NewGreen, after.g);
+            Assert.AreEqual(before.b, after.b);
+            Assert.AreEqual(before.a, after.a);
+        }
+
+        [Test]
+        public void TestColorWithR()
+        {
+            // Arrange.
+            var before = new Color(R, G, B, A);
+            const float NewRed = 0.5f;
+
+            // Act.
+            var after = before.WithRed(NewRed);
+
+            // Assert.
+            Assert.AreEqual(NewRed, after.r);
+            Assert.AreEqual(before.g, after.g);
+            Assert.AreEqual(before.b, after.b);
+            Assert.AreEqual(before.a, after.a);
+        }
+
+        #endregion
+    }
+}
---
+++ Source/UnityQuery/Assets/UnityQuery/Scripts/Colors.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Colors.cs" company="Nick Prühs">
+//   Copyright (c) Nick Prühs. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace UnityQuery
+{
+    using UnityEngine;
+
+    public static class Colors
+    {
+        #region Public Methods and Operators
+
+        public static Color WithAlpha(this Color c, float newAlpha)
+        {
+            return new Color(c.r, c.g, c.b, newAlpha);
+        }
+
+        public static Color WithBlue(this Color c, float newBlue)
+        {
+            return new Color(c.r, c.g, newBlue, c.a);
+        }
+
+        public static Color WithGreen(this Color c, float newGreen)
+        {
+            return new Color(c.r, newGreen, c.b, c.a);
+        }
+
+        public static Color WithRed(this Color c, float newRed)
+        {
+            return new Color(newRed, c.g, c.b, c.a);
+        }
+
+        #endregion
+    }
+}
